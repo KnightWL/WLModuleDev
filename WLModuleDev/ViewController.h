db1cf56f05579ba9ddfb60f06bd9435e686ce652
wanglei <cocoawanglei@foxmail.com>
2018-04-09 17:56:59 +0800

    Initial Commit

---
+++ WLModuleDev/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WLModuleDev
+//
+//  Created by fantasineer on 2018/4/9.
+//  Copyright © 2018年 fantasineer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WLModuleDev/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WLModuleDev
+//
+//  Created by fantasineer on 2018/4/9.
+//  Copyright © 2018年 fantasineer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
